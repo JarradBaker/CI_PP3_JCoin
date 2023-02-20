515fb9913db7cf05042ab1e580c9abb9566de821
JarradBaker <jarradbaker@outlook.com>
2023-02-20 13:59:54 +0000

    Add: simple comments for working with sheets

--- main.py
+++ main.py
@@ -7,10 +7,42 @@ scopes = [
 ]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "/workspace/CI_PP3_JCoin/secret_key/secret_key.json", scopes=scopes)
+    "secret_key/secret_key.json", scopes=scopes)
+
+file = gspread.authorize(creds)
+workbook = file.open("blockchain")
+sheet = workbook.sheet1
 
 
-def show_main_menu():
+# READING FROM THE SPREADSHEET
+
+# ITERATE THROUGH GIVEN CELLS
+# for cell in sheet.range('A2:A6'):
+#     print(cell.value)
+
+# PRINT A GIVEN CELL
+# print(sheet.acell('A3').value)
+
+# PRINT A WHOLE GIVEN COLUMN
+# print(sheet.col_values(1))
+
+# PRINT A WHOLE GIVEN ROW
+# print(sheet.row_values(2))
+
+
+# WRITING TO THE SPREADSHEET
+
+# UPDATE A GIVEN CELL BY CALL VALUE
+# sheet.update_acell('A2', 'Connor')
+
+# UPDATE A GIVEN CELL BY CELL COORDINATES (row, col, value)
+# sheet.update_cell(3, 2, 10) 
+
+# UPDATE A RANGE OF CELLS
+# sheet.update('A2:C2', [['alex', 'alex123', 10000]])
+
+
+def show_start_menu():
     print(" _________     _______    ________    _______    _______   __ ")
     print("|___   ___|   |  _____|  |  ____  |  |__   __|  |   _   | |  |")
     print("    | |       | |        | |    | |     | |     |  | |  | |  |")
@@ -18,3 +50,6 @@ def show_main_menu():
     print("    | |       | |        | |    | |     | |     |  | |  | |  |")
     print(" ___| |       | |_____   | |____| |   __| |__   |  | |  |_|  |")
     print("|_____|       |_______|  |________|  |_______|  |__| |_______|")
+
+
+show_start_menu()
