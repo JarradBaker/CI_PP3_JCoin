b3684729b924ec8f803823573ff53f8ac1bb9148
JarradBaker <jarradbaker@outlook.com>
2023-03-09 01:26:24 +0000

    Add: different files for sheets and menu

---
+++ gsheets.py
@@ -0,0 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "secret_key/secret_key.json", scopes=scopes)
+
+file = gspread.authorize(creds)
+workbook = file.open("blockchain")
+sheet = workbook.sheet1
