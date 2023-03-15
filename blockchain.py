14f1bde9d07e0c454b54859a1d8e7b61ba26e2f8
JarradBaker <jarradbaker@outlook.com>
2023-03-15 13:42:38 +0000

    Add: More exception handling

--- blockchain.py
+++ blockchain.py
@@ -2,19 +2,29 @@ import gsheets
 
 
 def send_coins(sending_user, receiving_user, amount):
+    """
+    A function for sending coins to another user. It takes
+    the sending amount off of the senders balance, and adds
+    it to the receivers balance.
+    """
     # sending_user wallet balance - amount
     sender_balance = int(gsheets.sheet.cell(sending_user, 3).value)
     receiver_balance = int(gsheets.sheet.cell(receiving_user, 3).value)
 
-    if (sender_balance - amount < 0):
-        # Exit the function and output error not enough balance
-        print("\nBalance is lower than amount to send ")
-        print("\nPlease send a smaller amount")
+    try:
+        if (sender_balance - amount < 0):
+            # Exit the function and output error not enough balance
+            raise ValueError('\nBalance is lower than amount to send\
+                \nPlease send a smaller amount\n')
+        else:
+            gsheets.sheet.update_cell(
+                sending_user, 3, sender_balance - amount)
+            gsheets.sheet.update_cell(
+                receiving_user, 3, receiver_balance + amount)
+            print("\nCoins have been sent successfully")
+    except ValueError as e:
+        print(e)
         return
-    else:
-        gsheets.sheet.update_cell(sending_user, 3, sender_balance - amount)
-        gsheets.sheet.update_cell(receiving_user, 3, receiver_balance + amount)
-        print("\nCoins have been sent successfully")
     # creates a new block
 
 
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import blockchain
 row_ID = 0
 incorrect_attempts = 0
 
+
 # READING FROM THE SPREADSHEET
 
 # Searching for a value in a cell in the specified column
@@ -104,11 +105,11 @@ def show_welcome_menu(incorrect_attempts):
             SystemExit(0)
         else:
             if not user_choice:
-                raise ValueError('\nNothing was entered')
+                raise ValueError('\nNothing was entered\n')
             else:
-                raise ValueError('\nInput didn\'t match a choice')
+                raise ValueError('\nInput didn\'t match a choice\n')
     except ValueError as e:
-        print("\n", e)
+        print(e)
         show_welcome_menu(incorrect_attempts)
 
 
@@ -122,40 +123,46 @@ def show_wallet_menu(row_ID):
     print("\n5. Exit the program")
     user_choice = input("\nPlease enter 1, 2, 3, 4, or 5 and press enter: ")
 
-    if (user_choice == "1"):
-        blockchain.check_balance(row_ID)
-        show_wallet_menu(row_ID)
-    elif (user_choice == "2"):
-        sender_row_ID = row_ID
-        receiver_wallet = input("Please choose a user to send coins to: ")
-        amount_to_send = int(input("Please choose an amount of coins: "))
-        # Check if receiver_wallet is null
-        if (gsheets.sheet.find(receiver_wallet)):
-            receiver_row_ID = gsheets.sheet.find(
-                receiver_wallet, in_column=1).row
-            blockchain.send_coins(
-                sender_row_ID, receiver_row_ID, amount_to_send)
-            next_empty_cell = gsheets.next_empty_row_transactions()
-            # Updating the transactions sheet
-            sender_wallet = gsheets.sheet.acell('A'+str(row_ID)).value
-            gsheets.transactions.update_cell(
-                next_empty_cell, 1, sender_wallet)
-            gsheets.transactions.update_cell(
-                next_empty_cell, 2, receiver_wallet)
-            gsheets.transactions.update_cell(
-                next_empty_cell, 3, amount_to_send)
+    try:
+        if (user_choice == "1"):
+            blockchain.check_balance(row_ID)
             show_wallet_menu(row_ID)
+        elif (user_choice == "2"):
+            sender_row_ID = row_ID
+            receiver_wallet = input("Please choose a user to send coins to: ")
+            amount_to_send = int(input("Please choose an amount of coins: "))
+            # Check if receiver_wallet is null
+            if (gsheets.sheet.find(receiver_wallet)):
+                receiver_row_ID = gsheets.sheet.find(
+                    receiver_wallet, in_column=1).row
+                blockchain.send_coins(
+                    sender_row_ID, receiver_row_ID, amount_to_send)
+                next_empty_cell = gsheets.next_empty_row_transactions()
+                # Updating the transactions sheet
+                sender_wallet = gsheets.sheet.acell('A'+str(row_ID)).value
+                gsheets.transactions.update_cell(
+                    next_empty_cell, 1, sender_wallet)
+                gsheets.transactions.update_cell(
+                    next_empty_cell, 2, receiver_wallet)
+                gsheets.transactions.update_cell(
+                    next_empty_cell, 3, amount_to_send)
+                show_wallet_menu(row_ID)
+            else:
+                print("\nCannot find reciever wallet")
+                show_wallet_menu(row_ID)
+        # elif (user_choice == 3):
+            # Do this
+        # elif (user_choice == 4):
+            # Do this
+        elif (user_choice == "5"):
+            SystemExit(0)
         else:
-            print("\nCannot find reciever wallet")
-            show_wallet_menu(row_ID)
-    # elif (user_choice == 3):
-        # Do this
-    # elif (user_choice == 4):
-        # Do this
-    elif (user_choice == "5"):
-        SystemExit(0)
-    else:
-        print("\nPlease enter one of the options")
+            if not user_choice:
+                raise ValueError('\nNothing was entered\n')
+            else:
+                raise ValueError('\nInput didn\'t match a choice\n')
+    except ValueError as e:
+        print(e)
         show_wallet_menu(row_ID)
 
 
